69abb9badb6129b0608e450b0fb0abb07480848c
TIKIYEON <tarikyorulmaz818@gmail.com>
2024-05-20 01:31:13 +0200

    init

---
+++ db/seed.py
@@ -0,0 +1,54 @@
+import psycopg2
+import os
+import csv
+import glob
+
+count = 0
+
+# db connection
+conn = psycopg2.connect(
+        dbname="stocks",
+        user="postgres",
+        password=os.getenv("POSTGRES_PASSWORD"),
+        host="localhost",
+        port="5432"
+    )
+
+cur = conn.cursor()
+
+# Dir of the csv files
+data_dir = "./data"
+
+# Import the csv files
+def seed_data(file, sid, stock_id):
+    with open(file, 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            cur.execute("""
+            INSERT INTO StockElements (sid, date, adj_close, close, open, low, high, stock_id)
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
+            """, (
+                    sid,
+                    row['Date'],
+                    row['Adj_close'],
+                    row['Close'],
+                    row['Open'],
+                    row['Low'],
+                    row['High'],
+                    stock_id
+            ))
+    conn.commit()
+
+# For every file create a new stock and for each row in the file insert the datainto stockelements
+for file in glob.glob(f"{data_dir}/*.csv"):
+    stock_id = os.path.basename(file).split(".")[0]
+    cur.execute("""
+    INSERT INTO Stocks (sid)
+    VALUES (%s)
+    """, (stock_id))
+    seed_data(file, count, stock_id)
+    count += 1
+
+# Close the connection
+cur.close()
+conn.close()
---
+++ db/stock.sql
@@ -0,0 +1,48 @@
+CREATE DATABASE stocks;
+
+CREATE TABLE Users (
+    uid SERIAL PRIMARY KEY,
+    password VARCHAR(255) NOT NULL,
+    mail VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(20)
+);
+
+/* 
+ 
+ */
+CREATE TABLE Portfolios (
+    pid SERIAL PRIMARY KEY,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES Users(uid)
+);
+
+CREATE TABLE PortfolioElements (
+    peid SERIAL PRIMARY KEY,
+    date DATE,
+    price_change VARCHAR(50),
+    purcase_time TIME,
+    portfolio_id INT,
+    stock_id INT,
+    FOREIGN KEY (stock_id) REFERENCES Stocks(sid),
+    FOREIGN KEY (portfolio_id) REFERENCES Portfolios(pid)
+);
+
+/*
+Use to reference the relevant stock elements form purchase 
+time to the current date
+*/
+CREATE TABLE Stocks (
+    sid SERIAL PRIMARY KEY,
+);
+
+CREATE TABLE StockElements (
+    sid SERIAL PRIMARY KEY,
+    date DATE,
+    adj_close DECIMAL(10, 2),
+    close DECIMAL(10, 2),
+    open DECIMAL(10, 2),
+    low DECIMAL(10, 2),
+    high DECIMAL(10, 2),
+    stock_id INT,
+    FOREIGN KEY (stock_id) REFERENCES Stocks(sid)
+);
