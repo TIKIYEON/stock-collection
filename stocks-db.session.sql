4bb7f2176380a9f146a49ac7bd8375f6fff1e33c
TIKIYEON <tarikyorulmaz818@gmail.com>
2024-06-04 22:15:16 +0200

    refactor: Update database connection details and add seed data for Stocks table

--- stocks-db.session.sql
+++ stocks-db.session.sql
@@ -1,4 +1,4 @@
 SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';
 
-SELECT * FROM stockelements LIMIT 10;
+SELECT * FROM StockElements LIMIT 10;
 SELECT * FROM stocks;
