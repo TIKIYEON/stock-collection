1513d41399942786723204b1d51d1f618abad5ac
TIKIYEON <tarikyorulmaz818@gmail.com>
2024-06-06 13:54:49 +0200

    May work now

--- stocks-db.session.sql
+++ stocks-db.session.sql
@@ -1,4 +1,5 @@
 SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';
 
 SELECT * FROM StockElements LIMIT 10;
+SELECT * FROM StockElements ORDER BY stock_id DESC LIMIT 10;
 SELECT * FROM stocks;
