ffac240c1aa4d023bf2c83bdccbeda513bc2269c
Fred <bgv600@alumni.ku.dk>
2024-06-08 12:48:09 +0200

    start of frontend
    ;)

--- backend/main.go
+++ backend/main.go
@@ -15,6 +15,19 @@ func init() {
 
 func main() {
 	r := gin.Default()
+	r.Use(func(c *gin.Context) {
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Accept")
+        c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(200)
+            return
+        }
+        c.Next()
+    })
+	r.Static("/static", "./frontend")
+
 	controllers.UserControllerRegister(&r.RouterGroup)
 	controllers.StockControllerRegister(&r.RouterGroup)
 	controllers.StockElementsControllerRegister(&r.RouterGroup)
