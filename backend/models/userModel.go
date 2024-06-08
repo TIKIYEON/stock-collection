5b93df7b2db62cdba7c256be88b27ceabb11eabe
TIKIYEON <tarikyorulmaz818@gmail.com>
2024-06-08 14:01:38 +0200

    refactor: Normalize API endpoint for retrieving user portfolios.
    Cannot populate database with a new table portfioliostocks from the relating go model for now idk why.

--- backend/models/userModel.go
+++ backend/models/userModel.go
@@ -1,10 +1,11 @@
 package models
 
 type User struct {
-	UID         uint   `gorm:"primaryKey;column:uid" json:"uid"`
-	Password    string `gorm:"not null" json:"password"`
-	Mail        string `gorm:"not null" json:"mail"`
-	PhoneNumber string `json:"phone_number"`
+	UID         uint        `gorm:"primaryKey;column:uid" json:"uid"`
+	Password    string      `gorm:"not null" json:"password"`
+	Mail        string      `gorm:"not null" json:"mail"`
+	PhoneNumber string      `json:"phone_number"`
+	Portfolios  []Portfolio `gorm:"foreignKey:UserID;references:UID" json:"portfolios"`
 }
 
 func (User) TableName() string {
